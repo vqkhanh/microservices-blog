f6bcd5c22c1ea81ecb6e75afe20f542f9de9b72f
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-08-23 00:33:30 +0700

    33. Emitting Events

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const uuid = require("uuid");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,7 +13,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = uuid.v4();
   const { title } = req.body;
 
@@ -20,6 +21,18 @@ app.post("/posts", (req, res) => {
     id,
     title,
   };
+
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "PostCreate",
+      data: {
+        id,
+        title,
+      },
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 
   res.status(201).send(posts[id]);
 });
