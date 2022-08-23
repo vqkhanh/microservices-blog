05fc65f5a335a66b623fbf242093952962f196e2
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-08-23 10:20:37 +0700

    38. Using the Query Service

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,21 +1,6 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    console.log(res);
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-  }, []);
-
+const CommentList = ({ comments }) => {
   // console.log(posts);
   const renderComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
